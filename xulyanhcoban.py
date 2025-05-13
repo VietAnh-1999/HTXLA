255837f6fcd81ebdfb3a6815fe9e51cd73a90b2d
VietAnh-1999 <nguyenvietanhtdh4@gmail.com>
2025-05-13 14:49:05 +0700

    va

--- xulyanhcoban.py
+++ xulyanhcoban.py
@@ -1,6 +1,11 @@
 import cv2
 print(cv2.__version__)
-path = r"D:\5.HT\HTVAXLA\picture\picture1"
-img = cv2.imread('picture1.png')
-cv2.imshow(img)
-cv2.waitKey(0)
+path = r"D:\5.HT\HTVAXLA\picture\picture1.png"  # duong dan
+img = cv2.imread(path)
+#img = cv2.imread('picture1.jpg')                #doc anh
+cv2.imshow("",img)                        #hiem thi anh
+cv2.waitKey(0)                               #thoi gian hien thi
+
+(h, w, d) = img.shape
+print(f"rong = {w}|cao = {h}|sau = {d}")
+
