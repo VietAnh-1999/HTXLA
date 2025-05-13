bb02fcf025af8f1b6b2c28d7b2188d83ca0565ee
VietAnh-1999 <nguyenvietanhtdh4@gmail.com>
2025-05-13 15:35:21 +0700

    Va

--- xulyanhcoban.py
+++ xulyanhcoban.py
@@ -7,5 +7,21 @@ cv2.imshow("",img)                        #hiem thi anh
 cv2.waitKey(0)                               #thoi gian hien thi
 
 (h, w, d) = img.shape
-print(f"rong = {w}|cao = {h}|sau = {d}")
+print(f"rong = {w}|cao = {h}|sau = {d}")      #doc kich thuoc anh
+r = 600/w
+dim = (600,int(h*r))
+resized = cv2.resize(img,dim)
+cv2.imshow("",resized)
+cv2.waitKey(0)
+(R, G, B) = img[60, 60]
+print(f"{R},{G},{B}")
 
+cut_picture = img[0:300, 60:300]
+cv2.imshow("",cut_picture)
+cv2.waitKey(0)
+# xoay anh
+center = (w//2, h//2)
+M = cv2.getRotationMatrix2D(center, 45 ,1.0)
+rotated = cv2.warpAffine(img, M,(w,h))
+cv2.imshow("",rotated)
+cv2.waitKey(0)
