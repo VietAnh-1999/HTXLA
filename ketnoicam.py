7cccb394b0ca309b4a73ddfc4a0ef88f1fc3d60f
VietAnh-1999 <nguyenvietanhtdh4@gmail.com>
2025-05-14 09:24:44 +0700

    Va

--- ketnoicam.py
+++ ketnoicam.py
@@ -5,6 +5,15 @@ while True:
     _, image = cap.read()
     (w,h,r) = image.shape
     #resize_img = cv2.resize(image,(int(w*1.5),int(h*1.5)))
+    cat_cas = cv2.CascadeClassifier('nguoi.xml')
+    cat_img = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    cats = cat_cas.detectMultiScale(cat_img)
+
+    for(x,y,w,h) in cats:
+        image = cv2.rectangle(image,(x,y),(x+w,y+h), (0,0,0),2)  # ve o vuong ảnh detect
+        cv2.putText(image,"Nguoi",(x,y),cv2.FONT_ITALIC,0.5,(0,0,255))   # viet chu  vao khung
+
     cv2.imshow("video",image)
+    
     if cv2.waitKey(1) == ord('q'):
         break
